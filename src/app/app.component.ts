3789c35c7e545c403fbe4d264a88a4b5ef9dcece
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-02-22 22:19:20 +0200

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-arr = [1, 1, 2, 3, 5, 8, 13]
-
-objs = [
-  {title: 'Post 1', author: 'Max', comments: [
-    {name: 'John', text: 'lorem 1'},
-    {name: 'John', text: 'lorem 2'},
-    {name: 'John', text: 'lorem 3'},
-  ]},
-  {title: 'Post 2', author: 'Max2', comments: [
-    {name: 'John2', text: 'lorem 1'},
-    {name: 'John2', text: 'lorem 2'},
-    {name: 'John2', text: 'lorem 3'},
-  ]},
-]
-
+  now: Date = new Date();
 }
