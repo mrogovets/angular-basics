220428ab6bae18e62651e21bf158b01c0a9207c1
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-02-24 22:22:17 +0200

    Part 3. Lesson 8. ChangeDetectionStrategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface Post {
   title: string;
@@ -11,11 +11,23 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   posts: Post[] = [
     {title: 'I want to study Angular components', text: 'I am studing Angular components', id: 1},
-    {title: 'Next block', text: 'It will be about directives and pipes', id: 2}
+    // {title: 'Next block', text: 'It will be about directives and pipes', id: 2}
   ];
+
+  ngOnInit(): void {
+    setTimeout(() =>{
+      console.log('Timeout')
+      this.posts[0] = {
+        title: 'Changed',
+        text: 'Changed 2',
+        id: 33
+      }
+    }, 5000)
+  }
+
   updatePosts(post: Post) {
     this.posts.unshift(post);
     // console.log('Post', post)
