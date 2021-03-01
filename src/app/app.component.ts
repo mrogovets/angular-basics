effd39cbdef3cd8b8bea195380d3e5bc5ed398c6
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-03-01 20:19:44 +0200

    filtering of list with pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  search = '';
+  searchField = 'title';
+
+  posts: Post[] = [
+    { title: 'Beer', text: 'The best beer in the world' },
+    { title: 'Bread', text: 'The best bread in the world' },
+    { title: 'Javascript', text: 'The best language in the world' },
+  ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MultByPipe } from './pipes/mult-by.pipe';
 import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, MultByPipe, ExMarksPipe],
+  declarations: [AppComponent, MultByPipe, ExMarksPipe, FilterPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
