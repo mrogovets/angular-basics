fbb431cf46d29f56c10f5ed62bdcd19efd53989c
mrogovets <mrogovets@ukr.net>
2021-03-15 20:56:29 +0200

    LocalCounterService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppCounterService } from './services/app-counter.service';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CounterComponent],
   imports: [BrowserModule, FormsModule],
   // providers: [AppCounterService],
   providers: [],
