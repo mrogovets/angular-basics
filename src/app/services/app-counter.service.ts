80c2b1897dbead4b38354652236d3677cd8db7c1
mrogovets <mrogovets@ukr.net>
2021-03-15 21:04:28 +0200

    constructor(private logservice: LogService) {}

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LogService } from './log.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AppCounterService {
   counter = 0;
+
+  constructor(private logservice: LogService) {}
+
   increase() {
+    this.logservice.log('increase counter...');
     this.counter++;
   }
   decrease() {
+    this.logservice.log('decrease counter...');
     this.counter--;
   }
 }
