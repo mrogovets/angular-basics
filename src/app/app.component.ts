74d91a734054662a06a15c58e36c703299e89f45
mrogovets <mrogovets@ukr.net>
2021-03-16 20:07:30 +0200

    created controls on form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,10 +9,16 @@ import { FormGroup } from "@angular/forms";
 export class AppComponent implements OnInit {
   form: FormGroup;
 
-  ngOnInit() {
-    this.form = new FormGroup({});
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(""),
+      password: new FormControl(null),
+    });
   }
   submit() {
-    console.log("Form submitted: ", this.form);
+    console.log("Form: ", this.form);
+    const formData = { ...this.form.value };
+
+    console.log("Form Data", formData);
   }
 }
