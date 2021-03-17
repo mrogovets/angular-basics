9275afef5202e0a0b7af32e3f56fb8f0e2e7c7f3
mrogovets <mrogovets@ukr.net>
2021-03-17 20:49:46 +0200

    created async validator

--- src/app/my.validators.ts
+++ src/app/my.validators.ts
@@ -1,4 +1,5 @@
 import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
 
 export class MyValidators {
   static restrictedEmails(control: FormControl): { [key: string]: boolean } {
@@ -8,5 +9,19 @@ export class MyValidators {
       };
     }
     return null;
+  }
+
+  static uniqEmail(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (control.value === "async@mail.ru") {
+          resolve({
+            uniqEmail: true,
+          });
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
   }
 }
