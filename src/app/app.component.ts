40f685bfd86660c0e4ee815d25ed8bef170cc112
mrogovets <mrogovets@ukr.net>
2021-03-18 19:11:46 +0200

    get data from server & put it in form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import {Component, OnInit} from '@angular/core'
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent implements OnInit {
-
-  ngOnInit() {
-
-  }
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
 }
 
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos?_limit=3")
+      .subscribe((todos) => {
+        console.log("Response", todos);
+        this.todos = todos;
+      });
+  }
+}
