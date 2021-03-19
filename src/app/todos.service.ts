c841a33e9a7d81e413197d0b8c32253eb4416fa7
mrogovets <mrogovets@ukr.net>
2021-03-19 21:53:14 +0200

    paravetr Observe

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, throwError } from "rxjs";
-import { catchError, delay } from "rxjs/operators";
+import { catchError, delay, map, tap } from "rxjs/operators";
 
 export interface Todo {
   completed: boolean;
@@ -34,8 +39,13 @@ export class TodoService {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos", {
         // params: new HttpParams().set("_limit", "3"),
         params,
+        observe: "response",
       })
       .pipe(
+        map((response) => {
+          // console.log("Response", response);
+          return response.body;
+        }),
         delay(500),
         catchError((error) => {
           console.log("Error: ", error.message);
@@ -44,16 +54,31 @@ export class TodoService {
       );
   }
 
-  removeTodo(id: number): Observable<void> {
-    return this.http.delete<void>(
-      `https://jsonplaceholder.typicode.com/todos/${id}`
-    );
+  removeTodo(id: number): Observable<any> {
+    return this.http
+      .delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        observe: "events",
+      })
+      .pipe(
+        tap((event) => {
+          if (event.type === HttpEventType.Sent) {
+            console.log("Sent", event);
+          }
+
+          if (event.type === HttpEventType.Response) {
+            console.log("Response", event);
+          }
+        })
+      );
   }
-  completeTodo(id: number): Observable<Todo> {
+  completeTodo(id: number): Observable<any> {
     return this.http.put<Todo>(
       `https://jsonplaceholder.typicode.com/todos/${id}`,
       {
         completed: true,
+      },
+      {
+        responseType: "json",
       }
     );
   }
