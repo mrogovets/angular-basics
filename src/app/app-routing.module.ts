2770fd80db4065f673ff09dbd33e656f440409b1
mrogovets <mrogovets@ukr.net>
2021-03-28 20:48:33 +0300

    added dynamic parametrs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
+import { PostComponent } from "./post/post.component";
 import { PostsComponent } from "./posts/posts.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "posts", component: PostsComponent },
+  { path: "posts/:id", component: PostComponent },
 ];
 
 @NgModule({
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Post, PostsService } from "../posts.service";
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  selector: "app-post",
+  templateUrl: "./post.component.html",
+  styleUrls: ["./post.component.scss"],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  post: Post;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private postsService: PostsService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log("Params", params);
+      this.post = this.postsService.getById(+params.id);
+    });
+  }
+  loadPost() {
+    this.router.navigate(["/posts", 44]);
+  }
 }
