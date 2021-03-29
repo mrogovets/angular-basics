28da6b46c66ededfd797ba245fd7f02efe60c449
mrogovets <mrogovets@ukr.net>
2021-03-29 21:07:12 +0300

    Part 10. Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
-import { AuthService } from "./auth.service";
+import {Component} from '@angular/core'
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private auth: AuthService) {}
+  constructor() {}
 }
+
