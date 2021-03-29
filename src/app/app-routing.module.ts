5815b8d4ef7313b6c2e4d5fd36b377b3324fac00
mrogovets <mrogovets@ukr.net>
2021-03-29 19:45:36 +0300

    inputing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AboutExtraComponent } from "./about-extra/about-extra.component";
 import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { PostComponent } from "./post/post.component";
@@ -7,7 +8,11 @@ import { PostsComponent } from "./posts/posts.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
+  {
+    path: "about",
+    component: AboutComponent,
+    children: [{ path: "extra", component: AboutExtraComponent }],
+  },
   { path: "posts", component: PostsComponent },
   { path: "posts/:id", component: PostComponent },
 ];
