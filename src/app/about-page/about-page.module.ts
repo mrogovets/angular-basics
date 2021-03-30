dc786d9cd3c0cbc0d40903c9188c22574b6c4977
mrogovets <mrogovets@ukr.net>
2021-03-30 21:59:02 +0300

    created file AboutPageModule

---
+++ src/app/about-page/about-page.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+import { AboutExtraPageComponent } from "./about-extra-page/about-extra-page.component";
+import { AboutPageComponent } from "./about-page.component";
+
+@NgModule({
+  declarations: [AboutPageComponent, AboutExtraPageComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: "about",
+        component: AboutPageComponent,
+        children: [{ path: "extra", component: AboutExtraPageComponent }],
+      },
+    ]),
+  ],
+  exports: [RouterModule],
+})
+export class AboutPageModule {}
